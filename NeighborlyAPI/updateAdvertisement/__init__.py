c42bbf3f7d9fa2312a726aa5347886ab663b4c94
Seth Samuel <samset2305@gmail.com>
2021-10-09 22:53:47 +0100

    commit to master

--- NeighborlyAPI/updateAdvertisement/__init__.py
+++ NeighborlyAPI/updateAdvertisement/__init__.py
@@ -9,9 +9,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if request:
         try:
-            url = "localhost"  # TODO: Update with appropriate MongoDB connection information
+            url = "mongodb://demop-cosmosdb:01m3tYIqchzi27lyG4xviqkqYAjfOlriFcY1j5Gs3mM6XIe1qgF0y6qJW7UdvIv0MUyEytpl8gPZGQtkS1AREw==@demop-cosmosdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@demop-cosmosdb@"  # TODO: Update with appropriate MongoDB connection information
             client = pymongo.MongoClient(url)
-            database = client['azure']
+            database = client['demop-cosmosdb']
             collection = database['advertisements']
             
             filter_query = {'_id': ObjectId(id)}
